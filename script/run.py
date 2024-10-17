7c3502fe0f13a87f90e16c4a381bd588651c6193
TSY244 <1241407552@qq.com>
2024-10-18 01:27:56 +0800

    c++ part1 finished

--- script/run.py
+++ script/run.py
@@ -4,7 +4,13 @@ import sys
 
 files=os.listdir("../src")
 for file in files:
-    cmd="cd ../src && g++ -o ../target/"+file[:-3]+".exe "+file
+    exe_name=None
+    if sys.platform=="win32":
+        exe_name=file[:-3]+".exe"
+    else:
+        exe_name=file[:-3]
+
+    cmd="cd ../src && g++ -o ../target/"+exe_name+" "+file
     os.system(cmd)
 
 print("finished!!")
